a641727bc918a31fb704dbff18169ac7a707a091
alexx19 <alexx198906@gmail.com>
2020-03-16 21:51:44 -0500

    creacion del docker **demo**

---
+++ app/usercontroller.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+#No hacer en produccion 0.0.0.0
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
