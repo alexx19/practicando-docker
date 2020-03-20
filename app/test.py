d419fc88b8d23ac6dbebe8bb56740bc2bd2470be
alexx19 <alexx198906@gmail.com>
2020-03-19 22:46:44 -0500

    fix module

--- app/test.py
+++ app/test.py
@@ -5,17 +5,17 @@ import usercontroller
 class TestDockerapp(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app.test_client()
+        self.usercontroller = usercontroller.app.test_client()
 
     def test_save_value(self):
-        response = self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
+        response = self.usercontroller.post('/', data=dict(submit='save', key='2', cache_value='two'))
         assert response.status_code == 200
         assert b'2' in response.data
         assert b'two' in response.data
 
     def test_load_value(self):
-        self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
-        response = self.app.post('/', data=dict(submit='load', key='2'))
+        self.usercontroller.post('/', data=dict(submit='save', key='2', cache_value='two'))
+        response = self.usercontroller.post('/', data=dict(submit='load', key='2'))
         assert response.status_code == 200
         assert b'2' in response.data
         assert b'two' in response.data
